a1c9663cc407ab7ddd38bd2d045600304cd3c950
simonmdsn <43004140+simonmdsn@users.noreply.github.com>
2019-11-25 10:24:56 +0100

    End coordinate is now last coordinate.

--- server.js
+++ server.js
@@ -6,29 +6,33 @@ app.listen(port);
 
 var geometry = require('spherical-geometry-js');
 
-app.get('/calculate/:startLat/:startLng/:endLat/:endLng',(req,res,next) => {
+//Variety in speed???
+app.get('/calculate/:startLat/:startLng/:endLat/:endLng', (req, res, next) => {
 
   //Take params from the url and uses them as params for LatLng objects
-  let start = new geometry.LatLng(req.params.startLat,req.params.startLng);
-  let end = new geometry.LatLng(req.params.endLat,req.params.endLng);
-  
-  //Returns distance between points in meters
-  let distance = geometry.computeDistanceBetween(start,end);
+  let start = new geometry.LatLng(req.params.startLat, req.params.startLng);
+  let end = new geometry.LatLng(req.params.endLat, req.params.endLng);
 
-  //Duration of the flight in seconds
-  let seconds = distance / 14;
+  //Returns distance between points in meters
+  let distance = geometry.computeDistanceBetween(start, end);
+
+  //14 equals the drones speed in m/s
+  let droneSpeed = 14;
+
+  //Duration of the flight in seconds,
+  let seconds = distance / droneSpeed;
 
   //Array of coordinates
   let coordinates = [];
   
   //Creates coordinate for each second.
-  for (let i = 0; i < seconds; i++) {
+  for (let i = 0; i == seconds; i++) {
     var coordinate = geometry.interpolate(start,end,i/seconds);
     var lat = coordinate.lat();
     var lng = coordinate.lng();
     coordinates.push([lat,lng]);
   }
-  
+
   //Sends requests
   res.send(coordinates);
 })
